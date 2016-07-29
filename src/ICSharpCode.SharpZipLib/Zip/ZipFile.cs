d78d1fc76a5ca4cc2e19d023808c6b2c4d80f696
hypersw <baltic@hypersw.net>
2016-07-29 14:45:54 +0300

    Process extra data only if we need immediate props from it, other stuff will be read on-demand (as we can read date on-demand now). This takes out entering ProcessExtraData for most common cases.

--- src/ICSharpCode.SharpZipLib/Zip/ZipFile.cs
+++ src/ICSharpCode.SharpZipLib/Zip/ZipFile.cs
@@ -3648,7 +3648,9 @@ namespace ICSharpCode.SharpZipLib.Zip
 					entry.ExtraData = extra;
 				}
 
-				entry.ProcessExtraData(false);
+				// Process extra data only if we need immediate props from it, other stuff will be read on-demand
+				if((entryrecord.size == UInt32.MaxValue) || (entryrecord.csize == UInt32.MaxValue) || (entryrecord.offset == UInt32.MaxValue) || (entryrecord.method == (int)CompressionMethod.WinZipAES))
+					entry.ProcessExtraData(true);
 
 				if (entryrecord.commentLen > 0)
 				{
