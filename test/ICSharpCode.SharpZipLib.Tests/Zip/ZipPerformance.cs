bed718acefbd0edc5acc5f651fd61aa2bfbdad60
Konstantin Kretov <konstantin.kretov@jetbrains.com>
2026-04-14 13:20:49 +0200

    MRI-4197 Ignored tests that aren't relevant any more

--- test/ICSharpCode.SharpZipLib.Tests/Zip/ZipPerformance.cs
+++ test/ICSharpCode.SharpZipLib.Tests/Zip/ZipPerformance.cs
@@ -20,12 +20,14 @@ namespace ICSharpCode.SharpZipLib.Tests.Zip
 		private static readonly PackageName[] LargePackageIds = {new PackageName("nalin", new SemanticVersion("1.0.0")), new PackageName("cryptopp", new SemanticVersion("5.6.3.4")), new PackageName("AWSSDKCPP-EC2", new SemanticVersion("0.13.20151001.8")), new PackageName("opencvcuda-debug.redist", new SemanticVersion("3.1.0")), new PackageName("opencvcuda-debug.symbols", new SemanticVersion("3.1.0")), new PackageName("Microsoft.Xbox.CloudCompute_110_md.SDK", new SemanticVersion("1502.26002")), new PackageName("opencvcuda-release.redist", new SemanticVersion("3.1.0")), new PackageName("opencvcuda-release.symbols", new SemanticVersion("3.1.0")), new PackageName("PayPal.Forms", new SemanticVersion("2.14.4")), new PackageName("Accusoft.ImageGear.PDF", new SemanticVersion("22.2.42")), new PackageName("Emgu2.9.0Beta", new SemanticVersion("2.9.2")), new PackageName("VDK.EmguCV.x64", new SemanticVersion("2.4.10.1")), new PackageName("Emgu_2.9.0_Beta_x64_1922", new SemanticVersion("1.0.0")), new PackageName("VVVV.EmguCV", new SemanticVersion("2.4.2.1")), new PackageName("Microsoft.Xbox.CloudCompute_110_mt.SDK", new SemanticVersion("1502.26004")), new PackageName("checkpushhuge", new SemanticVersion("9.0.1")), new PackageName("BigImage", new SemanticVersion("20.0.0")), new PackageName("Apitron.PDF.Kit", new SemanticVersion("1.0.38")), new PackageName("StrawberryPerl64", new SemanticVersion("5.22.2.1")), new PackageName("eMaxDataEssential", new SemanticVersion("3.0.0")), new PackageName("CEF3NativeDep", new SemanticVersion("3.2526.1366")), new PackageName("ZChat", new SemanticVersion("1.0.0")), new PackageName("Microsoft.Xbox.CloudCompute_120_mt.SDK", new SemanticVersion("1502.26003")), new PackageName("Microsoft.Xbox.CloudCompute_120_md.SDK", new SemanticVersion("1502.26002")), new PackageName("RadaeePDF_SDK_10", new SemanticVersion("1.9.10")), new PackageName("myEmguCV.Net", new SemanticVersion("0.0.12")), new PackageName("aarribillaga.architecture.web", new SemanticVersion("1.0.1")), new PackageName("Rust", new SemanticVersion("0.11.20140519")), new PackageName("VDK.EmguCV.x86", new SemanticVersion("2.4.10")), new PackageName("xcomponent.community", new SemanticVersion("4.5.0"))};
 
 		[Test]
+		[Ignore("This test relies on a local copy of some packages, which are not available on CI")]
 		public void OpenDirectoryMemStream()
 		{
 			DoReadDirectory(GetZipStreams().Cast<Stream>().ToList());
 		}
 
 		[Test]
+		[Ignore("This test relies on a local copy of some packages, which are not available on CI")]
 		public void OpenDirectoryWrappedStream()
 		{
 			DoReadDirectory(GetZipStreams().Select(ms => new WrappedStream(ms)).Cast<Stream>().ToList());
